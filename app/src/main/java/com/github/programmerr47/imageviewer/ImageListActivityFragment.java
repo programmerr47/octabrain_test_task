0d038de288452d0744239945c0d6bae2a3155742
ms <mikhail.spitsin@sidenis.com>
2015-12-02 15:48:43 +0300

    Init commit

---
+++ app/src/main/java/com/github/programmerr47/imageviewer/ImageListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.github.programmerr47.imageviewer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ImageListActivityFragment extends Fragment {
+
+    public ImageListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_image_list, container, false);
+    }
+}
